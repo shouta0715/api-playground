d013fc7f3f361915079ec776795b8390811f0270
shouta0715 <Myheroes0715@gmail.com>
2024-02-15 20:06:52 +0900

    segment config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,63 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const wait = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
 
-export default app
+app.get('/default', async (c) => {
+  console.log('\n----------------------- Default -----------------------\n');
+  await wait(3000);
+  const now = new Date().toISOString();
+  return c.json({ now });
+});
+
+app.get('/ssr', async (c) => {
+  console.log('\n----------------------- SSR -----------------------\n');
+  await wait(3000);
+  const now = new Date().toISOString();
+  return c.json({ now });
+});
+
+app.get('/ssg', async (c) => {
+  console.log('\n----------------------- SSG -----------------------\n');
+
+  await wait(3000);
+  const now = new Date().toISOString();
+  return c.json({ now });
+});
+
+app.get('/isr', async (c) => {
+  console.log('\n----------------------- ISR -----------------------\n');
+  await wait(3000);
+  const now = new Date().toISOString();
+  return c.json({ now });
+});
+
+app.get('/dynamic-function', async (c) => {
+  console.log(
+    '\n----------------------- Dynamic Function -----------------------\n'
+  );
+  await wait(3000);
+  const now = new Date().toISOString();
+  return c.json({ now });
+});
+
+app.get('/params', async (c) => {
+  console.log('\n----------------------- Params -----------------------\n');
+  const params = [1, 2, 3, 4, 5];
+
+  return c.json({ params });
+});
+
+app.get('/params/:id', async (c) => {
+  await wait(3000);
+  const id = c.req.param('id');
+
+  return c.json({ id });
+});
+
+export default app;
